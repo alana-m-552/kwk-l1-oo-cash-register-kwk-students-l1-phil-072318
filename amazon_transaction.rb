e4ca37e938cd16f5eb9efbe18ef1f0f83d6f63a9
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-30 20:26:38 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,35 @@
 # Code your cash register here!
+#creates two different users
+puts "do you have Amazon prime?"
+prime_user = "prime"
+user = "reg"
+
+password = false
+while password == false 
+  puts "input password"
+  pass = gets.chomp 
+  
+  if pass == prime_user
+  puts "correct password"
+  end 
+  
+  elsif pass == user
+  puts "correct password"
+  end 
+  
+  else pass = true
+  puts "incorrect password"
+  sleep (1)
+end 
+end
+#method for transaction
+def trans(item,cost)
+  puts "are you sure you would like to purchase #{item} for $#{cost}"
+end 
+
+#Give shoppers choices
+puts "what would you like to buy"
+puts "1. gucci tablet => 3000"
+puts "2. gucci drone => 2500"
+puts "3. gucci desk => 600"
+buy = gets.chomp 
